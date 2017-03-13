76670aa99f7b391724147ae2aebae8d8395c9887
invaderuc <gerarduc@gmail.com>
2017-03-13 13:50:26 -0300

    Cambio en el archivo fotter

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,6 +5,7 @@
 		<li><a href="#">Home</a></li>
 		<li><a href="#">About</a></li>
 		<li><a href="#">Contact</a></li>
+		<li><a href="#">Twitter</a></li>
 		<li class="float-right">Copyright 2016</li>
 	</ul>
 </div>
